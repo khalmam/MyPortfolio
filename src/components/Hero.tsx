06091c11ad5bb098041e8db1a0318047146a9a1f
khalmam <167781390+khalmam@users.noreply.github.com>
2025-07-10 12:10:33 +0100

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -37,13 +37,25 @@ const Hero = () => {
           </div>
 
           <div className="flex justify-center space-x-6 mb-12">
-            <a href="https://github.com/khalmam/" className="text-gray-600 hover:text-blue-600 transition-colors duration-200">
+            <a href="https://github.com/khalmam/" 
+               className="text-gray-600 hover:text-blue-600 transition-colors duration-200"
+               target="_blank"          // 
+               rel="noopener noreferrer"
+              >
               <Github size={24} />
             </a>
-            <a href="https://www.linkedin.com/in/ibrahim-imam-64b512a8/" className="text-gray-600 hover:text-blue-600 transition-colors duration-200">
+            <a href="https://www.linkedin.com/in/ibrahim-imam-64b512a8/" 
+               className="text-gray-600 hover:text-blue-600 transition-colors duration-200"
+               target="_blank" 
+               rel="noopener noreferrer"
+              >
               <Linkedin size={24} />
             </a>
-            <a href="imamkhaleel2@gmail.com" className="text-gray-600 hover:text-blue-600 transition-colors duration-200">
+            <a href="imamkhaleel2@gmail.com"   
+               className="text-gray-600 hover:text-blue-600 transition-colors duration-200"
+               target="_blank"          
+               rel="noopener noreferrer"
+              >
               <Mail size={24} />
             </a>
           </div>
